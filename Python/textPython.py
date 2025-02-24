2318d6cc984883862537ce9582119f4e4f3fbb82
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-24 18:33:07 -0500

    Created test script for C# ptyhon interop trials and error

---
+++ Python/textPython.py
@@ -0,0 +1,9 @@
+﻿import sys
+import time
+if __name__ == '__main__':
+    sys.stdout = sys.__stdout__
+    print('Starting')
+    time.sleep(1)
+    print('After 1s : Test, args : ')
+    for arg in sys.argv[1:]:
+        print(arg)
