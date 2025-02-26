bed65b1b5b1350eaa6acce2a330f357a4b37c86a
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-26 14:50:54 -0500

    Managed to work a live Python Caller and stream reader using simple python print and async C# stream read
    Also experimented with custom python logger with auto flush, currently commented in the test file but could have some use actually, even if setting the stdout bufferin to 1 and working with print is enough, we could use custom logging if we want specific behaviour, like flushing after a full generation has been outputed or stuff like this

---
+++ Python/testPython.py
@@ -0,0 +1,40 @@
+﻿import sys
+import time
+import logging
+
+# Create a logger
+#logger = logging.getLogger('my_logger')
+#logger.setLevel(logging.INFO)
+
+# Create a stream handler
+class FlushStreamHandler(logging.StreamHandler):
+    def emit(self, record):
+        super().emit(record)
+        self.flush()
+
+#buffering = 1 means line buffering, i.e console output is flushed after each line, i.e. it can be read externally
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1, encoding='cp1252', closefd=False)
+sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1, encoding='cp1252', closefd=False)
+
+# Create a stream handler
+#stream_handler = FlushStreamHandler(sys.stdout)
+#stream_handler.setLevel(logging.INFO)
+
+# Create a formatter and set it for the handler
+#formatter = logging.Formatter('%(message)s')
+#stream_handler.setFormatter(formatter)
+#
+# Add the handler to the logger
+#logger.addHandler(stream_handler)
+
+if __name__ == '__main__':
+    print('Starting')
+    #sys.stdout.flush()
+    time.sleep(5)
+    print('After 1s : Test, args : ')
+    #sys.stdout.flush()
+    for arg in sys.argv[1:]:
+        time.sleep(1)
+        print(arg)
+        #sys.stdout.flush()
+
